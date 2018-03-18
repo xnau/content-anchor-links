ed4e591a896d1b95ccbc4c5db29599d96718538b
roland <webdesign@xnau.com>
2018-03-17 16:00:18 -1000

    added the query to the 'wp_link_query' filter callback #10 version to 1.6

--- content-anchor-links.php
+++ content-anchor-links.php
@@ -4,7 +4,7 @@
  * Plugin Name: Content Anchor Links
  * Description: Easily add content anchors to your pages and posts
  * Author: Roland Barker, xnau webdesign
- * Version: 1.5
+ * Version: 1.6
  * Author URI: https://xnau.com
  * License: GPL3
  * Text Domain: content-anchor-links
@@ -14,8 +14,8 @@
 
 spl_autoload_register( 'xnau_content_anchor_links_autoload' );
 
-add_filter( 'content_edit_pre', array('xnau_WP_Headings_IDs', 'add_heading_ids'), 5, 2 );
-add_filter( 'wp_link_query', array('xnau_WP_Anchor_Links', 'get_content_anchors') );
+add_filter( 'content_edit_pre', array( 'xnau_WP_Headings_IDs', 'add_heading_ids'), 5, 2 );
+add_filter( 'wp_link_query', array( 'xnau_WP_Anchor_Links', 'get_content_anchors'), 10, 2 );
 
 function xnau_content_anchor_links_autoload( $class )
 {
