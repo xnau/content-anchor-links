e1282af33c58ac0ca8d5b2cd8532a58f345394d7
roland <webdesign@xnau.com>
2018-11-14 17:20:39 -1000

    headings in ACF wysiwyg fields now included #15

--- content-anchor-links.php
+++ content-anchor-links.php
@@ -15,6 +15,7 @@
 spl_autoload_register( 'xnau_content_anchor_links_autoload' );
 
 add_filter( 'content_edit_pre', array( 'xnau_WP_Headings_IDs', 'add_heading_ids'), 5, 2 );
+add_filter( 'acf/load_value/type=wysiwyg', array( 'xnau_WP_Headings_IDs', 'add_heading_ids'), 5, 2 );
 add_filter( 'wp_link_query', array( 'xnau_WP_Anchor_Links', 'get_content_anchors'), 10, 2 );
 
 function xnau_content_anchor_links_autoload( $class )
